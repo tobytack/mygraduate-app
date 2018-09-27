d54d970c529a93263b4e26381040b278ed73f0d3
Takuya Tobisawa <shxt0424@gmail.com>
2018-09-28 01:05:36 +0900

    backInit

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,29 +1,20 @@
 class FavoritesController < ApplicationController
   def create
     favorite = current_user.favorites.create(contact_id: params[:contact_id])
-    redirect_to contacts_url, notice: "#{favorite.contact.user.name}さんの投稿をお気に入りしました"
+    redirect_to contacts_url, notice: "#{favorite.contact.user.name}さんの投稿をストックしました"
   end
 
   def destroy
     favorite = current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to contacts_url, notice: "#{favorite.contact.user.name}さんの投稿をお気に入り解除しました"
+    redirect_to contacts_url, notice: "#{favorite.contact.user.name}さんの投稿をストック解除しました"
   end
 
   def show
     @user = User.find(session[:user_id])
     @favorite_contacts = @user.favorite_contacts
   end
-  
-
-  
-
-  def show_favorites
-    @contacts = Contact.find(params[:id])
-    @favorites = Favorite.where(contact_id: @contact.id).all
-  end
-  private
+end
 
   def contact_params
     params.require(:contact).permit(:title,:content, :image, :image_cache)
   end
-end
