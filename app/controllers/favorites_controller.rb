f87570f34b4963550f8362b6f0b0d6bd3d91f02b
Takuya Tobisawa <shxt0424@gmail.com>
2018-09-28 08:03:05 +0900

    contact_path2

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,9 +1,9 @@
 class FavoritesController < ApplicationController
-  skip_before_action :verify_authenticity_token ,:only => [:create]
+  
   
   def create
-    favorite = current_user.favorites.create(contact_id: params[:contact_id])
-    redirect_to contacts_url, notice: "#{favorite.contact.user.name}さんの投稿をお気に入りしました"
+    @favorite = current_user.favorites.create(contact_id: params[:contact_id])
+    
   end
 
   def destroy
